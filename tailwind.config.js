8a1220eec6153e84513fc62ad817839905160ca4
S. Stottelaar <stottelaar.s@gmail.com>
2024-09-30 09:23:44 +0200

    Upgraded to v5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
     content: [
         "./resources/**/*.antlers.html",
+        "./resources/**/*.antlers.php",
         "./resources/**/*.blade.php",
         "./resources/**/*.vue",
         "./content/**/*.md",
     ],
+
     theme: {
-        fontFamily: {
-            sans: ["satoshi", "sans-serif"],
-        },
-        extend: {},
     },
-    plugins: [require("@tailwindcss/typography")],
+
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/forms"),
+    ],
 };
